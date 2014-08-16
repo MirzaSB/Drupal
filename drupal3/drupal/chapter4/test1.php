3e93440e68dd31fa4ec7d6e34cbd8cdc6e4b4642
Mirza <mirzabaig@5TS502117968ML.local>
2014-08-16 04:15:20 -0400

    completed chapter 4

---
+++ drupal3/drupal/chapter4/test1.php
@@ -0,0 +1,24 @@
+<?php
+
+$peach = 'ugly';
+
+if ($peach == 'good') {
+  print 'This peach is very good!';
+}
+elseif ($peach == 'bad') {
+	print "This peach is lame.";
+}
+elseif ($peach == 'ugly') {
+	print "This peach is ugly.";
+}
+else {
+	print 'Sorry, the peach rating is not valid';
+}
+
+/*
+if ($peach == 'bad') {
+  print 'This peach is lame.';
+}
+*/
+
+?>
